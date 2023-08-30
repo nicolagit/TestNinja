4dd9d2b995d3b6f78230613b916f2bc054fecb01
Nicola <nyc.zyp@gmail.com>
2023-08-30 22:56:19 +0100

    1.20 - Writing a Simple Unit Test

---
+++ TestNinja.Test/MathTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.Test
+{
+    [TestFixture]
+    public class Math_Tests
+    {
+        [Test]
+        public void Add_WhenCalled_RetursTheSumOfArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.That(result.Equals(3));
+        }
+    }
+}
